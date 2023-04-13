9b6e4526f6ea09f1e9ce2a1f17c7c0468420894f
Jack Thacker <jackthacker89@gmail.com>
2023-04-13 11:49:19 +0100

    Test drive displayWeather function

--- weather.test.js
+++ weather.test.js
@@ -8,9 +8,11 @@ describe("weather class", () => {
   beforeEach(async () => {
     mockClient.fetchWeatherData.mockResolvedValueOnce({
       name: "London",
-      main: { temp: 12 },
+      main: { temp: 12, feels_like: 10, humidity: 78 },
+      weather: [{ main: "clouds" }, { main: "rain" }],
     });
     await weather.load("London");
+    jest.spyOn(console, "log");
   });
   it("", async () => {
     expect(weather.getWeatherData()).toEqual(
@@ -28,5 +30,14 @@ describe("weather class", () => {
       "The temperature in London is 12; the temperature in Sheffield is 15: Sheffield is warmer."
     );
   });
-  it("provides user-friendly breakdown of the weather in the terminal", () => {});
+  it("provides user-friendly breakdown of the weather in the terminal", () => {
+    weather.displayWeather();
+    expect(console.log).toHaveBeenCalledWith(`
+City:         London
+Weather:      clouds, rain
+Temperature:  12
+Feels like:   10
+Humidity:     78%
+    `);
+  });
 });
